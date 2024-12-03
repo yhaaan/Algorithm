bbc0f506c71a46fd7fd81960e3225c5e1a7f9fa8
yhaaan <122730802+yhaaan@users.noreply.github.com>
2024-12-03 14:42:46 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+
 using namespace std;
-void findnum(vector<int>&vec ,vector<pair<int,int>> &num,int n,int m);
-void pv(vector<int>&v);
-int arr[100000] = {0,};
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n,m;
     int num;
-    vector<int> a;
-    vector<pair<int,int>> b;
+    vector<int> a, b;
     cin >>n;
     for(int i=0;i<n;i++){
         cin >> num;
@@ -19,36 +20,10 @@ int main() {
     sort(a.begin(),a.end());
     for(int i=0;i<m;i++){
         cin >> num;
-        b.push_back(make_pair(num,i));
+        if(binary_search(a.begin(),a.end(),num))
+            cout<<'1'<<"\n";
+        else
+            cout<<'0'<<"\n"; 
     }
-    sort(b.begin(),b.end(),[](const pair<int,int> &a, const pair<int,int> &b){
-        return a.first < b.first;
-    });
-
-    findnum(a,b,n,m);
-    for(int i =0;i<m;i++)
-        cout << arr[i] <<"\n";
     return 0;
-}
-
-void findnum(vector<int>&vec ,vector<pair<int,int>> &num,int n,int m){
-    int i=0,j=0;
-    while(i<n && j<m){
-        if(vec[i] == num[j].first){
-            arr[num[j].second] = 1;
-            j++;
-        }
-        else if(vec[i] > num[j].first){
-            j++;
-        }
-        else{
-            i++;
-        }
-    }
-}
-
-void pv(vector<int>&v){
-    for (auto name : v) {
-        cout << name;
-    }
 }
