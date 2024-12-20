8bd8f70495f6381216c2d69dd254c26db34d6c3b
yhaaan <122730802+yhaaan@users.noreply.github.com>
2024-12-20 20:32:04 +0900

    [Silver II] Title: 스택 수열, Time: 1060 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -2,15 +2,38 @@
 #include <vector>
 using namespace std;
 
+int cur = 0;
+int maxn = 0;
+int *arr;
+vector<char> res;
 
+void up(int num){
+    for(int j=1;j <= num;j++){
+        if(arr[j] == 0){
+            res.push_back('+');
+            arr[j] = 1; //push 된적 있음
+        }
+    }
+    maxn = num;
+    cur = num;
+    res.push_back('-');
+    arr[num] = 2; //pop 된 적 있음
+}
+
+void down(int num){
+    for(int j=cur-1; j>=num ;j--){
+         if(arr[j] == 1){
+             res.push_back('-');
+             arr[j] = 2;
+         }   
+    }
+    cur = num;
+}
 
 int main() {
     int n,num;
     cin >> n;
-    int cur = 0;
-    int max = 0;
-    int *arr = new int[n+1];
-    vector<char> res;
+    arr = new int[n+1];
     for(int i =1; i<=n; i++) arr[i] = 0; 
 
     
@@ -20,27 +43,8 @@ int main() {
             cout << "NO";
             return 0;
         }
-        if(num > max){
-            for(int j=1;j <= num;j++){
-                if(arr[j] == 0){
-                    res.push_back('+');
-                    arr[j] = 1; //push 된적 있음
-                }
-            }
-            max = num;
-            cur = num;
-            res.push_back('-');
-            arr[num] = 2; //pop 된 적 있음
-        }
-        else{
-            for(int j=cur-1; j>=num ;j--){
-                 if(arr[j] == 1){
-                     res.push_back('-');
-                     arr[j] = 2;
-                 }   
-            }
-            cur = num;
-        }
+        if(num > maxn) up(num);
+        else down(num);   
     }
 
     for(auto v : res)
