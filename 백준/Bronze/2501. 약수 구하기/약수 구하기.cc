1bf5b7f84c2d67f416c8c4f9dc8ba5315dd11aab
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-01-05 19:17:29 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,k,cnt=0;
+    cin >> n >> k;
+    for(int i = 1; i<=n ;i++){
+        if(n%i == 0){
+            cnt ++;
+        }
+        if(cnt == k){
+            cout << i;
+            return 0;
+        }
+    }
+    cout << 0;
+    return 0;
+}
