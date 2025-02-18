788d838a0c45f885d68fb0c353a0faf2a9b2e10b
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-02-19 00:08:57 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    cout<<"       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._| "<<endl;
+       
+
+
+
+    
+}
