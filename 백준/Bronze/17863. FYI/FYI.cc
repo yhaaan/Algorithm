f136443ca684272926c84b39a6aef4aa9575947e
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-03-21 21:07:44 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    if(n/10000 == 555)
+        cout <<"YES";
+    else
+        cout <<"NO";
+}
+
