2e84c5babec49901d590dbad298828defd5ce401
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-04-03 00:42:17 +0900

    [Silver II] Title: 과일 탕후루, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[200010];
+int g[10];
+int answer;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    cin >> n;
+
+
+    for(int i =0 ; i< 10;i++)
+        g[i] = 0;
+    for(int i = 0; i < n ; i++){
+        cin >> arr[i];
+        
+    }
+    
+
+    int a, b;
+    a = 0;
+    b = -1;
+    answer = 0;
+
+    int cnt = 0;
+    
+    while(true){
+        //cout << "a : " <<a << " b : " << b << " ans : " <<answer << " c : " <<cnt  <<"\n";
+
+        if(answer <= 2){
+            b++;
+            if(g[arr[b]] == 0){
+                answer ++;
+            }   
+            g[arr[b]] ++;
+
+            if(answer <=2 )
+                cnt = cnt < b-a+1? b-a+1: cnt; //cnt 갱신
+                     
+            
+            //cout << "g["<<arr[b]<<"] = " << g[arr[b]] << endl;
+            
+
+        }
+        else if(answer > 2){
+            a++;
+            if(g[arr[a-1]] == 1){
+                answer --;
+            }
+             g[arr[a-1]] --;
+        }
+        
+        if(b >= n-1) break;
+        //cnt = check(a,b);
+        
+    }
+
+    //cout << answer <<endl;
+    cout << cnt;
+    //cout <<"\n a : " << a <<" b : " << b;
+    
+}
