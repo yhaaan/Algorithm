6485cb76e7fdf9e349258aca1e13198d1d022296
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-04-08 16:03:06 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n ; i++){
+        cin >> str;
+        cout << str.size() <<endl;
+        
+    }
+    
+}
