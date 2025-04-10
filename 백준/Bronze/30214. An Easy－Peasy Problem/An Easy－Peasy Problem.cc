69acf7812f41a614f994f0ab17661716a850c395
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-04-10 09:56:27 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float n , m;
+    cin >> n >> m;
+    if(n/m >=0.5){
+        cout << "E";
+    }
+    else {
+        cout << "H";
+    }
+}
