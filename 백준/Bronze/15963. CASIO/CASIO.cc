40e1a4e0b07ad70ceb655a793fd2e8ba24eb555d
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-05-02 01:31:24 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n , m;
+    cin >> n >> m;
+    if(n == m ){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+}
