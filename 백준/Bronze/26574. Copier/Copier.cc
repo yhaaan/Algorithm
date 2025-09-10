4192ea90e13f6e28be7365af65b440a941fe1111
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-09-10 18:56:21 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int k;
+    cin >> n;
+    for(int i = 0; i < n ; i++){
+        cin >> k;
+        cout << k <<" " << k <<endl;
+    }
+}
