3ff62a53829bce574e2e48e34b6be7a123f7fd48
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-09-21 00:19:26 +0900

    [Bronze V] Title: 13 Floors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int f; 
+    cin >> f;
+    if (f < 13) {
+        cout << f << endl;
+    } else {
+        cout << f + 1 << endl;
+    }
+    return 0;
+}
