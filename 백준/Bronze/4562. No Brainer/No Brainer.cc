6b15f0c0809e7cd103f80f4fefb6c2aff4b2fbb9
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-10-01 12:35:39 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a,b;
+    cin >> n;
+    for(int i =0;i< n ; i++){
+        cin >> a>>b;
+        if(a < b) cout << "NO BRAINS\n";
+        else cout << "MMM BRAINS\n";
+    }
+}
