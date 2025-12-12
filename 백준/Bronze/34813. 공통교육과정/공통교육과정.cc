7139362ecfca5d8bd9e239fc4323131934ddd4ec
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-12-12 13:21:29 +0900

    [Bronze V] Title: 공통교육과정, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include<string>
+
+int main() {
+    string str;
+    cin >> str;
+    if(str[0] == 'F') cout <<"Foundation";
+    if(str[0] == 'C') cout <<"Claves";
+    if(str[0] == 'V') cout <<"Veritas";
+    if(str[0] == 'E') cout <<"Exploration";
+    
+    
+}
