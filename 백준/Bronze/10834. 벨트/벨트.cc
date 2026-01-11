3e1e6c04419b96f679e1a34e3418293f9e2eb304
yhaaan <122730802+yhaaan@users.noreply.github.com>
2026-01-11 16:11:07 +0900

    [Bronze II] Title: 벨트, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.cc
+++ 백준/Bronze/10834. 벨트/벨트.cc
@@ -3,24 +3,19 @@ using namespace std;
 
 int main() {
     int m;
-    int k[1001];
-    k[0] = 1; // 처음꺼가 1번 돌떄 
+    double fac = 1;
+    int dir = 1;
     int a,b,c;
     cin >> m;
     for(int i = 0; i < m ; i++){
         cin >> a>>b>>c;
-        int t = 1;
-        if(c == 0) t = 1;
-        else t = -1;
-        k[i+1] = k[i] * b/a * t;
+        if(c == 1) dir = dir * -1;
+        
+        fac = fac * b/a;
     }
 
-    if(k[m] < 0){
-        cout << 1 <<" " <<k[m]*-1;
-    }
-    else{
-        cout << 0 <<" " <<k[m];
-    }
+    int ans = dir == -1 ? 1 : 0;
+    cout << ans << " " << (long long)fac;
     /*
     for(int i = 0 ; i <= m;i ++){
         cout << k[i] <<" ";
