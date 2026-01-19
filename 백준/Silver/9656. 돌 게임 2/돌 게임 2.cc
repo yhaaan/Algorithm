00b72bf1ca15878796f49488044a2302b1f633c4
yhaaan <122730802+yhaaan@users.noreply.github.com>
2026-01-19 10:51:45 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -3,15 +3,8 @@ using namespace std;
 
 int main() {
     int n;
-    bool dp[1001];
     cin >> n;
-    dp[1] = false; dp[2] = true; dp[3] = false;
-    for(int i = 4; i <=n; i++){
-        dp[i] = dp[i-2] ? true:false;
-    }
-
-  
-    if(dp[n]) cout <<"SK";
+    if(n%2 == 0) cout <<"SK";
     else cout << "CY";
     
 }
