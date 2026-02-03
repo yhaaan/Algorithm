3dab6400b7d8db32965d4a53b369580f7b34bbcd
yhaaan <122730802+yhaaan@users.noreply.github.com>
2026-02-03 11:09:41 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    string str2;
+    cin >> str;
+    cin >> str2;
+    if(str == str2){
+        cout << 0;
+    }
+    else{
+        cout <<1550;
+    }
+}
