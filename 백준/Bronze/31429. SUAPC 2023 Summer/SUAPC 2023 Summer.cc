32fc27313100c7b8e8bb824b6e0c95aa5c40b053
yhaaan <122730802+yhaaan@users.noreply.github.com>
2026-02-11 19:08:17 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    int solved[] = {12, 11, 11, 10, 9, 9, 9, 8, 7, 6, 6};
+    int penalty[] = {1600, 894, 1327, 1311, 1004, 1178, 1357, 837, 1055, 556, 773};
+
+    int n;
+    cin >> n;
+
+    cout << solved[n - 1] << " " << penalty[n - 1] << endl;
+
+    return 0;
+}
