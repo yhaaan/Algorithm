65d028fc3f1c3adf295ecf537e76db5213ecb256
yhaaan <122730802+yhaaan@users.noreply.github.com>
2026-03-13 19:07:48 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a>>b;
+    if( a== 2 && b == 18){
+        cout << "Special";
+    }
+    else if(a < 2 || a == 2 && b < 18){
+        cout << "Before";
+    }
+    else{
+        cout << "After";
+    }
+}
